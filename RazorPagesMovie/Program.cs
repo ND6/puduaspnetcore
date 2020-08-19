4d8c55d211b0f80c49dffe47ce3f53f87bba9a51
yanpengfei <noch1217@qq.com>
2020-08-19 16:48:04 +0800

    add Project RazorPagesMovies

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
+using Serilog;
+using System;
+
+namespace RazorPagesMovie
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("App start.");
+            try
+            {
+                logger.LogInformation("App init start.");
+
+                SeedData.Initialize(services);
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError(ex, "An error occurred seeding the DB.");
+            }
+            logger.LogInformation("App init end.");
+
+            host.Run();
+            logger.LogInformation("App  end.");
+
+            Log.CloseAndFlush();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseSerilog()
+        ;
+    }
+}
